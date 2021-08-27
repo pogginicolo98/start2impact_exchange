c9481b2c5e6237da1ae0db740f781ecee6a6c1a1
Nicolò Poggi <poggi.nicolo.srvz@zohomail.eu>
2021-08-27 20:02:29 +0200

    Add wallet model

--- bitcoinExchange/exchange/admin.py
+++ bitcoinExchange/exchange/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from exchange.models import Order, Profile
+from exchange.models import Order, Profile, Wallet
 
 admin.site.register(Order)
 admin.site.register(Profile)
+admin.site.register(Wallet)
--- bitcoinExchange/exchange/models.py
+++ bitcoinExchange/exchange/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from djongo import models
+from random import uniform
 
 
 class Profile(models.Model):
@@ -7,9 +8,7 @@ class Profile(models.Model):
     ???
     """
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    ips = models.Field(default=[])
-    subprofiles = models.Field(default={})
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
@@ -25,10 +24,12 @@ class Order(models.Model):
     ???
     """
 
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='orders')
     datetime = models.DateTimeField(auto_now_add=True)
     price = models.FloatField()
     quantity = models.FloatField()
+    type = models.BooleanField()
+    status = models.BooleanField(default=True)
 
     def __str__(self):
         return self.datetime.strftime("%d/%m/%Y, %H:%M:%S")
@@ -37,3 +38,21 @@ class Order(models.Model):
         verbose_name = 'Order'
         verbose_name_plural = 'Orders'
         ordering = ['-datetime']
+
+
+class Wallet(models.Model):
+    """
+    ???
+    """
+
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    dollars = models.FloatField(default=0)
+    bitcoins = models.FloatField(default=uniform(1, 10))
+
+    def __str__(self):
+        return str(self.profile)
+
+    class Meta:
+        verbose_name = 'Wallet'
+        verbose_name_plural = 'Wallets'
+        ordering = ['profile']
